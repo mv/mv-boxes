c21feafcd17bad143d3dda5b4328b1b7a76bf9b1
Marcus Vinicius Ferreira <ferreira.mv@gmail.com>
2013-12-22 22:49:29 -0200

    Minified: CentOS 6

---
+++ minify/minify-centos-6.sh
@@ -0,0 +1,171 @@
+###
+###
+###
+
+/usr/bin/yum -y remove \
+    abrt-addon-ccpp             \
+    abrt-addon-kerneloops       \
+    abrt-addon-python           \
+    abrt-cli                    \
+
+
+
+/usr/bin/yum -y remove \
+    aic94xx-firmware            \
+    atmel-firmware              \
+    b43-fwcutter                \
+    b43-openfwwf                \
+    bfa-firmware                \
+    ipw2100-firmware            \
+    ipw2200-firmware            \
+    ivtv-firmware               \
+    iwl100-firmware             \
+    iwl1000-firmware            \
+    iwl3945-firmware            \
+    iwl4965-firmware            \
+    iwl5000-firmware            \
+    iwl5150-firmware            \
+    iwl6000-firmware            \
+    iwl6000g2a-firmware         \
+    iwl6000g2b-firmware         \
+    iwl6050-firmware            \
+    libertas-usb8388-firmware   \
+    kernel-firmware             \
+    ql2100-firmware             \
+    ql2200-firmware             \
+    ql23xx-firmware             \
+    ql2400-firmware             \
+    ql2500-firmware             \
+    rt61pci-firmware            \
+    rt73usb-firmware            \
+    xorg-x11-drv-ati-firmware   \
+    zd1211-firmware             \
+
+
+
+/usr/bin/yum -y remove \
+    acpid                       \
+    alsa-utils                  \
+    biosdevname                 \
+    blktrace                    \
+    bridge-utils                \
+    dmraid                      \
+    dosfstools                  \
+    efibootmgr                  \
+    eject                       \
+    kbd                         \
+    kbd-misc                    \
+    libaio                      \
+    lvm2                        \
+    mdadm                       \
+    mtr                         \
+    parted                      \
+    pcmciautils                 \
+    pinfo                       \
+    pm-utils                    \
+    quota                       \
+    setserial                   \
+    smartmontools               \
+    sos                         \
+    strace                      \
+    sysstat                     \
+    usbutils                    \
+    wireless-tools              \
+
+
+
+/usr/bin/yum -y remove \
+    cpuspeed                    \
+    dmidecode                   \
+    irqbalance                  \
+    microcode_ctl               \
+    pciutils                    \
+    virt-what                   \
+
+
+
+/usr/bin/yum -y remove \
+    authconfig                  \
+    acl                         \
+    attr                        \
+    audit                       \
+    cryptsetup-luks             \
+    kexec-tools                 \
+    ntsysv                      \
+    rhnsd                       \
+    rng-tools                   \
+    setuptool                   \
+    system-config-firewall-tui  \
+    system-config-network-tui   \
+    systemtap-runtime           \
+    vconfig                     \
+    yum-plugin-security         \
+    yum-rhn-plugin              \
+    yum-utils                   \
+
+
+
+/usr/bin/yum -y remove \
+    iptables-ipv6               \
+    ntp                         \
+    ntpdate                     \
+    rdate                       \
+    rsync                       \
+    tcpdump                     \
+    tcp_wrappers                \
+    traceroute                  \
+    wget                        \
+
+
+
+/usr/bin/yum -y remove \
+    at                          \
+    bc                          \
+    centos-indexhtml            \
+    cyrus-sasl-plain            \
+    dbus                        \
+    ed                          \
+    fprintd-pam                 \
+    hunspell                    \
+    hunspell-en                 \
+    mlocate                     \
+    m4                          \
+    nano                        \
+    pam_passwdqc                \
+    psacct                      \
+    readahead                   \
+    rfkill                      \
+    tcsh                        \
+    tmpwatch                    \
+    words                       \
+    vim-enhanced                \
+    xz                          \
+
+
+
+/usr/bin/yum -y remove \
+    selinux-policy              \
+    selinux-policy-targeted     \
+
+
+
+# /usr/bin/yum -y remove \
+#     file                        \
+#     time                        \
+#     which                       \
+#     bzip2                       \
+#     unzip                       \
+#     zip
+
+
+
+# /usr/bin/yum -y remove \
+#     newt                        \
+#     newt-python                 \
+#     slang                       \
+#     ustr                        \
+
+
+
+# vim:ft=sh:
+
