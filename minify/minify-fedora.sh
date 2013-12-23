5d52c265fe6bdba28e8a4d54028b983f072ff453
Marcus Vinicius Ferreira <ferreira.mv@gmail.com>
2013-12-23 01:07:00 -0200

    Fedora 19: 216 packages.

---
+++ minify/minify-fedora.sh
@@ -0,0 +1,37 @@
+###
+###
+###
+
+/usr/bin/yum -y remove \
+
+
+/usr/bin/yum -y remove \
+    authconfig                  \
+    acl                         \
+    attr                        \
+    audit                       \
+
+
+/usr/bin/yum -y remove \
+    avahi           \
+    avahi-autoipd   \
+    avahi-libs      \
+    NetworkManager  \
+    NetworkManager-glib  \
+    dnsmasq         \
+    tcp_wrappers                \
+    wpa_supplicant  \
+
+
+/usr/bin/yum -y remove \
+    biosdevname                 \
+    kdb           \
+    kdb-misc      \
+    linux-firmware             \
+    man-db        \
+    parted                      \
+
+
+
+# vim:ft=sh:
+
